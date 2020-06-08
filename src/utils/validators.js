ad83feb3bcf7b75e8b60ded1b1ca5531ad304dd9
ditu474 <daniel_ramirez82181@elpoli.edu.co>
2020-06-07 19:36:05 -0500

    new validators implemented

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -12,3 +12,13 @@ exports.validatePassword = (password) => {
   let r = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/);
   return r.test(password);
 };
+
+exports.validateName = (name) => {
+  let r = new RegExp(/^[a-zA-ZÀ-ú]+(([',. -][a-zA-ZÀ-ú])?[a-zA-ZÀ-ú]*)*$/);
+  return r.test(name);
+};
+
+exports.validateMobileNumber = (number) => {
+  let r = new RegExp(/^3[0-9]{9}$/);
+  return r.test(number);
+};
